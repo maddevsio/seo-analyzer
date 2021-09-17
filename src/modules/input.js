a7806bc75c58e66bd1a9443f5ffb9152b7b8ba4c
denisoed <denisoed@gmail.com>
2021-09-17 18:54:25 +0600

    Added func for get DOM from HTML

--- src/modules/input.js
+++ src/modules/input.js
@@ -1,4 +1,5 @@
-const fs = require('fs');
+import fs from 'fs';
+import { JSDOM } from 'jsdom';
 
 class Input {
   constructor() {}
@@ -7,25 +8,47 @@ class Input {
     return new Promise(async (resolve, reject) => {
       if (!Array.isArray(files)) {
         console.log('Input data is not a array');
-        return reject();
+        reject();
       }
       if (files.length === 0) {
         console.log('Input data is empty');
-        return reject();
+        reject();
       }
-      const html = await this._getHtml(files);
-      console.log(html);
+      const listHTML = await this._getHtml(files);
+      const listDOM = await this._getDom(listHTML);
+      resolve(listDOM);
     });
   }
 
+  /**
+   * Get the html from file
+   * @param {*} files [path, path, ...]
+   * @returns {Promise.Array} ['<string>', '<string>', ...]
+   */
   _getHtml(files) {
     return new Promise((resolve, reject) => {
-      const html = [];
+      const listHtml = [];
       files.forEach((file) => {
-        const data = fs.readFileSync(file, 'utf8');
-        html.push(data);
+        const htmlText = fs.readFileSync(file, 'utf8');
+        listHtml.push(htmlText);
       });
-      resolve(html);
+      resolve(listHtml);
+    });
+  }
+
+  /**
+   * Transform the html to DOM
+   * @param {Array} listHtml [{<string>}, {<string>}, ...] 
+   * @returns {Promise.Array} [{<JSDOM>}, {<JSDOM>}, ...]
+   */
+  _getDom(listHtml) {
+    return new Promise((resolve, reject) => {
+      const doms = [];
+      listHtml.forEach((html) => {
+        let dom = new JSDOM(html);
+        doms.push(dom);
+      });
+      resolve(doms);
     });
   }
 }
