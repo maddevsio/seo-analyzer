c3c34a5f05939a684880f01ab6e5e3ecf454907c
denisoed <denisoed@gmail.com>
2021-09-24 11:48:15 +0600

    Moved CFonts func to Logger

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import defaultRules from './rules/index'
-import CFonts from 'cfonts'
+import defaultRules from './rules/index';
 
 import Input from './modules/input'
 import Output from './modules/output'
@@ -31,19 +30,19 @@ class SeoAnalyzer {
 
   // ------- Input functions ------- // 
   inputFiles(files) {
-    this._showLogo();
+    this.logger.printTextToConsole('Seo Analyzer');
     this.inputData = this.input.files(files, this._ignoreFiles);
     return this;
   }
   
   inputUrls(urls) {
-    this._showLogo();
+    this.logger.printTextToConsole('Seo Analyzer');
     this.inputData = this.input.urls(urls);
     return this;
   }
   
   inputFolders(folders) {
-    this._showLogo();
+    this.logger.printTextToConsole('Seo Analyzer');
     this.inputData = this.input.folders(folders, this._ignoreFolders, this._ignoreFiles);
     return this;
   }
@@ -77,23 +76,6 @@ class SeoAnalyzer {
       callback(json);
       return this;
     })();
-  }
-
-  _showLogo() {
-    CFonts.say('Seo|Analyzer', {
-      font: 'block',              // define the font face
-      align: 'left',              // define text alignment
-      colors: ['system'],         // define all colors
-      background: 'transparent',  // define the background color, you can also use `backgroundColor` here as key
-      letterSpacing: 1,           // define letter spacing
-      lineHeight: 1,              // define the line height
-      space: true,                // define if the output text should have empty lines on top and on the bottom
-      maxLength: '0',             // define how many character can be on one line
-      gradient: false,            // define your two gradient colors
-      independentGradient: false, // define if you want to recalculate the gradient for each new line
-      transitionGradient: false,  // define if this is a transition between colors directly
-      env: 'node'                 // define the environment CFonts is being executed in
-    });
   }
 }
 
--- src/modules/logger.js
+++ src/modules/logger.js
@@ -1,3 +1,5 @@
+import CFonts from 'cfonts';
+
 class Analyzer {
   constructor() {
     this.red = '\x1b[31m';
@@ -6,12 +8,45 @@ class Analyzer {
     this.successMessage = 'SEO defects were not detected.\n';
   }
 
+  /**
+   * Print formatted result to console
+   * @param {Array} - Array of reports
+   * @returns {Error} - Stop execution and print error
+   */
   result(result) {
     this._logResult(result);
   }
 
+  /**
+   * Print error message to console
+   * @param {String} - Message
+   * @returns {String} - Print formatted message to console
+   */
   error(error) {
     this._logError(error);
+  }
+
+  /**
+   * Print beautiful message to console
+   * @param {String} - Result message
+   * @returns {String} - Print CFonts message to console
+   */
+  printTextToConsole(text) {
+    const formattedText = text.replace(' ', '|');
+    CFonts.say(formattedText, {
+      font: 'block',              // define the font face
+      align: 'left',              // define text alignment
+      colors: ['system'],         // define all colors
+      background: 'transparent',  // define the background color, you can also use `backgroundColor` here as key
+      letterSpacing: 1,           // define letter spacing
+      lineHeight: 1,              // define the line height
+      space: true,                // define if the output text should have empty lines on top and on the bottom
+      maxLength: '0',             // define how many character can be on one line
+      gradient: false,            // define your two gradient colors
+      independentGradient: false, // define if you want to recalculate the gradient for each new line
+      transitionGradient: false,  // define if this is a transition between colors directly
+      env: 'node'                 // define the environment CFonts is being executed in
+    });
   }
 
   /**
