88c58668e811132c234b3fdb7042a748c46e64ec
denisoed <denisoed@gmail.com>
2021-09-29 14:41:04 +0600

    Added tests for NoTooManyStrongTagsRule

--- src/rules/NoTooManyStrongTagsRule.js
+++ src/rules/NoTooManyStrongTagsRule.js
@@ -1,12 +1,10 @@
 function noTooManyStrongTagsRule(dom, options) {
   return new Promise(resolve => {
-    let report = '';
     const elements = dom.window.document.querySelectorAll('strong');
     if (elements && elements.length > options.threshold) {
-      report +=
-        'This HTML have more than ' + options.threshold + ' <strong> tags';
+      resolve(`This HTML have more than ${options.threshold} <strong> tags`);
     }
-    resolve(report);
+    resolve(null);
   });
 }
 
