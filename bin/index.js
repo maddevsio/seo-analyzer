efe6d1f2c9ee59524231d39a808961963b52eb33
Denisoed <denisoed@gmal.com>
2024-03-30 11:15:36 +0600

    Added files cli

--- bin/index.js
+++ bin/index.js
@@ -2,16 +2,21 @@
 const { Command } = require('commander');
 const ver = require('./version');
 const urls = require('./urls');
+const files = require('./files');
 
 const program = new Command();
 
 program
   .option('-v, --version', 'Show version', ver, '')
   .option('-u, --urls <urls...>', 'Multiple url analysis')
+  .option('-f, --files <files...>', 'Multiple files analysis')
   .parse(process.argv);
 
 const options = program.opts();
 
 if (options.urls) {
   urls(options.urls);
+}
+if (options.files) {
+  files(options.files);
 }
