2c137dfd3608dc8dd6ea4ec3364a1f400516df23
Denisoed <denisoed@gmal.com>
2024-03-31 12:24:13 +0600

    Added default values for rules with params

--- bin/index.js
+++ bin/index.js
@@ -7,16 +7,19 @@ const program = new Command();
 
 program
   .option('-v, --version', 'Show version', ver, '')
-  .option('-u, --urls <urls...>', 'Multiple url analysis')
-  .option('-f, --files <files...>', 'Multiple files analysis')
-  .option('-fl, --folders <folders...>', 'Multiple folders analysis')
+  // IGNORES
   .option('-iu, --ignore-urls <ignoreUrls...>', 'Multiple ignore urls')
   .option('-if, --ignore-files <ignoreFiles...>', 'Multiple ignore files')
-  .option('-r, --rules <rules...>', 'Multiple rules')
   .option(
     '-ifl, --ignore-folders <ignoreFolders...>',
     'Multiple ignore folders'
   )
+  // INPUTS
+  .option('-u, --urls <urls...>', 'Multiple url analysis')
+  .option('-f, --files <files...>', 'Multiple files analysis')
+  .option('-fl, --folders <folders...>', 'Multiple folders analysis')
+  // RULES
+  .option('-r, --rules <rules...>', 'Multiple rules')
   .parse(process.argv);
 
 const options = program.opts();
--- src/rules/MetaSocialRule.js
+++ src/rules/MetaSocialRule.js
@@ -1,22 +1,21 @@
+import { META_SOCIAL_RULE } from './config/defaults';
+
 function metaSocialRule(dom, options = { properties: [] }) {
   return new Promise(resolve => {
     const report = [];
-    if (options && options.properties && options.properties.length) {
-      options.properties.forEach(property => {
-        const element = dom.window.document.querySelector(
-          `head > meta[property="${property}"]`
+    const list = options?.properties || META_SOCIAL_RULE.properties;
+    list.forEach(property => {
+      const element = dom.window.document.querySelector(
+        `head > meta[property="${property}"]`
+      );
+      if (!element) {
+        report.push(`This HTML is missing a <meta property="${property}"> tag`);
+      } else if (!element.content) {
+        report.push(
+          `The content attribute for the <meta property="${property}" content=""> tag is empty`
         );
-        if (!element) {
-          report.push(
-            `This HTML is missing a <meta property="${property}"> tag`
-          );
-        } else if (!element.content) {
-          report.push(
-            `The content attribute for the <meta property="${property}" content=""> tag is empty`
-          );
-        }
-      });
-    }
+      }
+    });
     resolve(report);
   });
 }
